f5a08d25c49a1729c78ab47e5945a959df902473
Shulei <wang_shulei@foxmail.com>
2025-01-21 18:06:24 +0800

    fixup

--- src/md-render/rehype-plugins.ts
+++ src/md-render/rehype-plugins.ts
@@ -1,18 +1,6 @@
-import { Element, Root, RootContent } from 'hast'
+import { Element, Root } from 'hast'
+import { toString } from 'hast-util-to-string'
 import { visit } from 'unist-util-visit'
-
-function getMarkdownNodeInnerText(node: RootContent): string {
-    if (!node) {
-        return ''
-    }
-    if (node.type === 'text') {
-        return node.value
-    }
-    if (node.type === 'element') {
-        return node.children.map(getMarkdownNodeInnerText).join('')
-    }
-    return ''
-}
 
 /**
  * 给 markdown 的 h1, h2, h3 标签添加 id 属性
@@ -26,7 +14,7 @@ export const customRehypePlugin = () => {
         let headingIndex = 0
         visit(tree, 'element', (node: Element) => {
             if (node.tagName === 'h1' || node.tagName === 'h2' || node.tagName === 'h3') {
-                const innerText = getMarkdownNodeInnerText(node)
+                const innerText = toString(node)
                 const id = `${headingIndex}-${innerText}`
                 node.properties.id = id
                 headingIndex++
